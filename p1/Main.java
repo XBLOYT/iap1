53bd75e7e9ce63024228a951c5953ee554b860fa
miquel.torner.vinals <miquel.torner.vinals@estudiantat.upc.edu>
2022-10-08 19:47:01 +0200

    imports i reorganitzacio general

---
+++ p1/Main.java
@@ -0,0 +1,19 @@
+package p1;
+
+import IA.Energia.Central;
+import IA.Energia.Centrales;
+import IA.Energia.Cliente;
+import IA.Energia.Clientes;
+import IA.Energia.TestEnergia;
+import IA.Energia.VEnergia;
+
+public class Main {
+    
+    public Client getCetral(int id) {
+        Central[id].get();
+    }
+
+    public Client getClient(int id) {
+        Clients[id].get();
+    }
+}
