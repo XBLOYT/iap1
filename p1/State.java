44e0c4483cf7e2eee51177bb79d64a8017996965
xbloyt <xavierbern2@gmail.com>
2022-10-10 13:11:38 +0200

    Inici d'implementacio estat

--- p1/State.java
+++ p1/State.java
@@ -1,17 +1,30 @@
 package p1;
-
+import java.util.*;
 public class State {
+    private ArrayList<ArrayList<int>> assignacions;
+    private int[] capacitatrestant;
+    private double benefici;
+    //ArrayList<Central> Centrales; ?
+    //ArrayList<Cliente> Clientes; ?
+
 
     // Constructor
-    public State() {
+    public State(int numerocentrals, int[] costcentrals) {
+        assignacions = new ArrayList<>(numerocentrals);
+        for(int  i = 0; i < numerocentrals; ++i){
+            assignacions.add(new ArrayList());
+        }
+        capacitatrestant = new int[numerocentrals];
+        capacitatrestant = costcentrals; //Al principi tots els costos restants son 0.
+        benefici = 0;
+    }
+
+    public void swapConsumers(Cliente cl1, Cliente cl2, Central cen1, Central cen2, int nomcl1, int nomcl2, int nomcen1, int nomcen2) {
 
     }
 
-    public void swapConsumers(int c1, int c2) {
+    public void moveConsumer(int central, int consumer, Central cent, Cliente cl) {
 
-    }
-
-    public void moveConsumer(int central, int consumer) {
 
     }
 
